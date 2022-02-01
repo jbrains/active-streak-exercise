79713233f04d62121070c95cbd03bd46b1ec5b51
J. B. Rainsberger <me@jbrains.ca>
2022-02-01 17:41:54 -0400

    We handle a streak 0 days with 1 measurement as long as we're asking on 2022-02-01.

--- test/active-streak-spec.js
+++ test/active-streak-spec.js
@@ -1,11 +1,17 @@
 import { test } from "zora";
 import { DateTime } from "luxon";
 
+const isBeforeYesterday = (date) => date < DateTime.fromISO("2022-01-31");
 const activeStreak = (measurements) =>
-  measurements.length > 0 ? "1 day" : "0 days";
+  measurements.length > 0
+    ? isBeforeYesterday(DateTime.fromISO(measurements[0].date))
+      ? "0 days"
+      : "1 day"
+    : "0 days";
 
 test("end to end", (t) => {
   t.equal(activeStreak([]), "0 days");
   t.equal(activeStreak([{ date: "2022-02-01" }]), "1 day");
   t.equal(activeStreak([{ date: "2022-01-31" }]), "1 day");
+  t.equal(activeStreak([{ date: "2022-01-30" }]), "0 days");
 });
