eda38745761dbbaacd2a56132dab340cb5a2f31f
J. B. Rainsberger <me@jbrains.ca>
2022-02-01 17:45:33 -0400

    Improved a name by making it more precise.

--- test/active-streak-spec.js
+++ test/active-streak-spec.js
@@ -1,7 +1,7 @@
 import { test } from "zora";
 import { DateTime } from "luxon";
 
-const activeStreak = (measurements) =>
+const formatActiveStreak = (measurements) =>
   measurements.length === 0
     ? "0 days"
     : DateTime.fromISO(measurements[0].date) < "2022-01-31"
@@ -9,8 +9,8 @@ const activeStreak = (measurements) =>
     : "1 day";
 
 test("end to end", (t) => {
-  t.equal(activeStreak([]), "0 days");
-  t.equal(activeStreak([{ date: "2022-02-01" }]), "1 day");
-  t.equal(activeStreak([{ date: "2022-01-31" }]), "1 day");
-  t.equal(activeStreak([{ date: "2022-01-30" }]), "0 days");
+  t.equal(formatActiveStreak([]), "0 days");
+  t.equal(formatActiveStreak([{ date: "2022-02-01" }]), "1 day");
+  t.equal(formatActiveStreak([{ date: "2022-01-31" }]), "1 day");
+  t.equal(formatActiveStreak([{ date: "2022-01-30" }]), "0 days");
 });
