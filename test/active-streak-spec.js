006df8a9503fdc7c1885f15e536b25c1d14de821
J. B. Rainsberger <me@jbrains.ca>
2022-02-01 18:01:13 -0400

    Moved the changing code closer to the tests.

--- test/active-streak-spec.js
+++ test/active-streak-spec.js
@@ -1,21 +1,21 @@
 import { test } from "zora";
 import { DateTime } from "luxon";
 
+const formatDurationInDays = (n) => `${n} day${n === 1 ? "" : "s"}`;
+
+const formatActiveStreak = (asOfDateAsText) => (measurements) =>
+  formatDurationInDays(
+    activeStreak(DateTime.fromISO(asOfDateAsText))(
+      measurements.map((m) => DateTime.fromISO(m.date))
+    )
+  );
+
 const activeStreak = (asOfDate) => (datesSortedByMostRecent) =>
   datesSortedByMostRecent.length === 0
     ? 0
     : datesSortedByMostRecent[0] < asOfDate.minus({ days: 1 })
     ? 0
     : 1;
-
-const formatActiveStreak = (measurements) =>
-  formatDurationInDays(
-    activeStreak(DateTime.fromISO("2022-02-01"))(
-      measurements.map((m) => DateTime.fromISO(m.date))
-    )
-  );
-
-const formatDurationInDays = (n) => `${n} day${n === 1 ? "" : "s"}`;
 
 test("active streak", (t) => {
   const activeStreakFromDatesAsText = (datesAsText) =>
@@ -30,9 +30,9 @@ test("active streak", (t) => {
 });
 
 test("end to end", (t) => {
-  t.equal(formatActiveStreak([]), "0 days");
-  t.equal(formatActiveStreak([{ date: "2022-02-01" }]), "1 day");
-  t.equal(formatActiveStreak([{ date: "2022-01-31" }]), "1 day");
+  t.equal(formatActiveStreak("2022-02-01")([]), "0 days");
+  t.equal(formatActiveStreak("2022-02-01")([{ date: "2022-02-01" }]), "1 day");
+  t.equal(formatActiveStreak("2022-02-01")([{ date: "2022-01-31" }]), "1 day");
   t.ok(DateTime.fromISO("2022-01-30") < DateTime.fromISO("2022-01-31"));
-  t.equal(formatActiveStreak([{ date: "2022-01-30" }]), "0 days");
+  t.equal(formatActiveStreak("2022-02-01")([{ date: "2022-01-30" }]), "0 days");
 });
