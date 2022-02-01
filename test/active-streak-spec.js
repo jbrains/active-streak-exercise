4d2fb8c4dd70608603634b37a2e0e9fba6f9c6b3
J. B. Rainsberger <me@jbrains.ca>
2022-02-01 17:52:19 -0400

    Compare dates to dates.

--- test/active-streak-spec.js
+++ test/active-streak-spec.js
@@ -1,16 +1,23 @@
 import { test } from "zora";
 import { DateTime } from "luxon";
 
+const activeStreak = (datesSortedByMostRecent) =>
+  datesSortedByMostRecent.length === 0
+    ? 0
+    : DateTime.fromISO(datesSortedByMostRecent[0]) <
+      DateTime.fromISO("2022-01-31")
+    ? 0
+    : 1;
+
 const formatActiveStreak = (measurements) =>
-  measurements.length === 0
-    ? "0 days"
-    : DateTime.fromISO(measurements[0].date) < "2022-01-31"
-    ? "0 days"
-    : "1 day";
+  formatDurationInDays(activeStreak(measurements.map((m) => m.date)));
+
+const formatDurationInDays = (n) => `${n} day${n === 1 ? "" : "s"}`;
 
 test("end to end", (t) => {
   t.equal(formatActiveStreak([]), "0 days");
   t.equal(formatActiveStreak([{ date: "2022-02-01" }]), "1 day");
   t.equal(formatActiveStreak([{ date: "2022-01-31" }]), "1 day");
+  t.ok(DateTime.fromISO("2022-01-30") < DateTime.fromISO("2022-01-31"));
   t.equal(formatActiveStreak([{ date: "2022-01-30" }]), "0 days");
 });
